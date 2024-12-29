ec81f2e0e3ca1830695b738affaecb80d33fa1d8
Subuking001 <3subratmishra1sep@gmail.com>
2024-12-29 22:33:29 +0530

    logger file

--- src/exception.py
+++ src/exception.py
@@ -4,6 +4,7 @@
 
 import sys
 import os
+import logging
 
 def error_message_detail(error, error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()
@@ -19,8 +20,10 @@ def error_message_detail(error, error_detail:sys):
 
 class CustomException(Exception):
     def __init__(self, error_message, error_detail:sys):
-        super.__init__(error_message)
+        super().__init__(error_message)
         self.error_message = error_message_detail(error_message, error_detail=error_detail)
 
     def __str__(self):
         return self.error_message
+
+
